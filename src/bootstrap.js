1004d53643bec27ff0269cde2cdf8f64c6d75b49
Myth <zhouran1@kingsoft.com>
2018-01-16 15:51:11 +0800

    removed sw.js

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -41,14 +41,12 @@ document.onkeydown = function (e) {
   }
 }
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('sw.js').then(function (registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope)
-    }).catch(function (err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err)
-    })
+if ('serviceWorker' in navigator && window.location.protocol === 'https:') {
+  navigator.serviceWorker.register('service-worker.js').then(function (registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope)
+  }).catch(function (err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err)
   })
 }
--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -1,7 +1,7 @@
 <style lang="less" scoped>
 .title-bar {
   display: flex; font-size: 18px; height: 64px; line-height: 64px;
-  color: #222226; background: rgba(250,250,250,0.90);
+  color: #222226; background: rgba(250, 250, 250, 0.90);
   box-shadow: 0 1px 0 #DBDBDB; justify-content: flex-end;
 }
 .title {
@@ -32,4 +32,3 @@ export default {
   }
 }
 </script>
-
